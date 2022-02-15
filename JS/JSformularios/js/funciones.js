00175e4b86413fa4d768f7f7ae9d4d5b07209240
cesguicam <a_cesguicam@iestriana.com>
2022-02-15 22:49:38 +0100

    js

--- JS/JSformularios/js/funciones.js
+++ JS/JSformularios/js/funciones.js
@@ -2,9 +2,49 @@ function validar(campo) {
     //var nombre = document.getElementById("nombre");
     //alert(campo.value);
     if (campo.value == null || campo.value == "" || campo.value.length == 0) {
-       campo.style = "border: 2px solid red";
-       campo.focus(); 
+       //campo.style = "border: 2px solid red";
+       //campo.focus();
+       mostrarError("spnombre", "Error: Nombre vacio"); 
     }else {
-        campo.style = "";
+        //campo.style = "";
+        borrarError("spnombre");
     }
+
+}
+
+function mostrarError (nombreSpan, mensaje) {  //campo es spnombre por que en mostrar error sale lo hemos pueto el primero, y mensaje es error:..
+    document.getElementById(nombreSpan).innerHTML = mensaje;
+}
+function borrarError(nombreSpan){
+    document.getElementById(nombreSpan).innerHTML = "";
+}
+function validarFormulario(){
+    var cadenaError =  "";
+    var nombre = document.getElementById("nombre");
+    if (nombre.value == null || nombre.value.length == 0){
+        cadenaError = cadenaError + "<li>Error: Nombre vacio</li>";
+    }
+    var postal = document.getElementById("postal");
+    if (postal.value == null || nombre.value.length == 0) {
+        cadenaError = cadenaError + "<li>Error: cod postal vacio</li>";
+    }else if (postal.value.length != 5){
+        cadenaError = cadenaError + "<li>Error: Cod postal de 5 cifras</li>";
+    }
+
+    
+
+
+
+    if (cadenaError.length == 0) {
+        return true;
+    } else {
+        document
+        mostrarError("sperrores", cadenaError);
+        return false;
+    }
+
+    
+}
+function mostrarEdad(campoedad) {
+    document.getElementById("spedad").innerHTML = campoedad.value
 }
