f1e0e91cb4357a57ce794b41775dc7cdbf728c80
cesguicam <a_cesguicam@iestriana.com>
2022-03-03 13:14:41 +0100

    si

--- JS/ejer04/js/funciones.js
+++ JS/ejer04/js/funciones.js
@@ -1,10 +1,12 @@
 function calcular(){
      const n1 = document.getElementById("numero1").value;
-    const n2 = document.getElementById("numero2").value;
+     const n2 = document.getElementById("numero2").value;
 
 if(n1 > n2){
     document.getElementById("resultado").innerHTML = "El mayor numero es " + n1 + " y el menor es " + n2
 } else if (n1 < n2){
     document.getElementById("resultado").innerHTML = "El menor numero es " + n1 + " y el mayor es " + n2
+}else{
+    document.getElementById("resultado").innerHTML = n1+" y  " + n2 +" Son iguales"
 }
 }
