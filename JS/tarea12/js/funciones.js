d6e1522af2a8aec0939fb0e0e4de0ba6613f9664
cesguicam <a_cesguicam@iestriana.com>
2022-03-16 12:37:20 +0100

    ewt

--- JS/tarea12/js/funciones.js
+++ JS/tarea12/js/funciones.js
@@ -1,15 +1,34 @@
 function enviar(){
     // lo tengo que hacer con varios if(condiccion que me pida en el encunciado) y susu respectivos elses para mostrar el error si no se cumple lo que me ha pedido el enunciado, para esto tengo que hacer vaios span con id distintas para que salga el error debajo de la tabla
 var email=document.getElementById("email").value;
-var web=document.getElementById("web").value;
+var web=document.getElementById("Web").value;
 var pass=document.getElementById("password").value;
 var pass2=document.getElementById("password2").value;
+var erroremail="";
+var errorweb="";
+var erroredad="";
+var errorcontra="";
 
-if (email=!""&& email*="@" ){
-    document.getElementById("error1").innerHTML="Campo correcto"
+if (email=!""/*&& email*="@"*/ ){
+    document.getElementById("error1").innerHTML="Campo correcto";
+
 }else{
-    document.getElementById("error1").innerHTML="Tiene un error en el campo 'Email', sevise que tenga un '.' y una '@'. "
+    erroremail="Tiene un error en el campo 'Email', sevise que tenga un '.' y una '@'. "
+    document.getElementById("error1").innerHTML="Tiene un error en el campo 'Email', sevise que tenga un '.' y una '@'. ";
 }
-if(){}
+/*if(web){
+error2
+}else{
 
+}*/
+if(edad>0){
+    document.getElementById("error3").innerHTML="Campo correcto";
+}else{
+    document.getElementById("error3").innerHTML="La edad tiene que ser mayor que 0.";
+}
+if (pass==pass2){
+    document.getElementById("error5").innerHTML="Campo correcto";
+}else{
+    document.getElementById("error5").innerHTML="La contraseña no coincide, prueba de nuevo";
+}
 }
