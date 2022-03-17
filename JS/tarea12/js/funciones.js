ba3c9af7a7eb4783469616a3479de068115375ce
cesguicam <a_cesguicam@iestriana.com>
2022-03-17 14:10:03 +0100

    tarea 12

--- JS/tarea12/js/funciones.js
+++ JS/tarea12/js/funciones.js
@@ -1,34 +1,62 @@
-function enviar(){
-    // lo tengo que hacer con varios if(condiccion que me pida en el encunciado) y susu respectivos elses para mostrar el error si no se cumple lo que me ha pedido el enunciado, para esto tengo que hacer vaios span con id distintas para que salga el error debajo de la tabla
-var email=document.getElementById("email").value;
-var web=document.getElementById("Web").value;
-var pass=document.getElementById("password").value;
-var pass2=document.getElementById("password2").value;
-var erroremail="";
-var errorweb="";
-var erroredad="";
-var errorcontra="";
+function validar() {
+    var correcto = true;
+    var texto = "";
 
-if (email=!""/*&& email*="@"*/ ){
-    document.getElementById("error1").innerHTML="Campo correcto";
+    var res = document.getElementById("resultado");
+    res.innerHTML = "";
 
-}else{
-    erroremail="Tiene un error en el campo 'Email', sevise que tenga un '.' y una '@'. "
-    document.getElementById("error1").innerHTML="Tiene un error en el campo 'Email', sevise que tenga un '.' y una '@'. ";
-}
-/*if(web){
-error2
-}else{
+    var correo = document.getElementById("correo").value;
+    var web = document.getElementById("web").value;
+    var edad = document.getElementById("edad").value;
+    var pass = document.getElementById("pass").value;
+    var repass = document.getElementById("repass").value;
 
-}*/
-if(edad>0){
-    document.getElementById("error3").innerHTML="Campo correcto";
-}else{
-    document.getElementById("error3").innerHTML="La edad tiene que ser mayor que 0.";
-}
-if (pass==pass2){
-    document.getElementById("error5").innerHTML="Campo correcto";
-}else{
-    document.getElementById("error5").innerHTML="La contraseña no coincide, prueba de nuevo";
-}
+    /*if (correo.length == 0 || correo == "" 
+        || web.length == 0 || web == "" 
+        || edad.length == 0 || edad == "" 
+        || pass.length == 0 || pass == "" 
+        || repass.length == 0 || repass == "") {
+        texto += "<li>Debe rellenar todos los campos</li>";
+        correcto = false;
+    } else if (String(correo).indexOf("@") == -1 && 
+                String(correo).indexOf(".") == -1 && 
+                String(correo).indexOf("@") < String(correo).indexOf(".")) {
+    } */
+    
+    if (String(correo).indexOf("@") == -1) {
+        texto += "<li>@ necesaria en email</li>";
+        correcto = false;
+    } else {
+        var posA = String(correo).indexOf("@");
+        var posPunto = String(correo).indexOf(".", posA);
+        if (posPunto == -1 || posPunto == posA + 1 
+                    || posPunto == String(correo).length-1) {
+            texto += "<li>Punto incorrecto en email</li>";
+            correcto = false;
+        }
+    }
+
+    if (!String(web).startsWith("http://")) {
+        texto += "<li>Web debe comenzar por http://</li>";
+        correcto = false;
+    }
+
+    if (parseInt(edad) <= 0) {
+        texto += "<li>Edad incorrecta</li>";
+        correcto = false;
+    }
+
+    if (String(pass) != String(repass)) {
+        texto += "<li>Deben ser iguales</li>";
+        correcto = false;
+    }
+
+    if (String(pass).localeCompare(String(repass)) != 0) {
+        texto += "<li>Deben ser iguales</li>";
+        correcto = false;
+    }
+    
+    res.innerHTML = texto;
+
+    return correcto;
 }
